7b34f835a3f71b55ad055f2f146b6a1d73490779
yarovojigor1988 <yarovojigo1988@gmail.com>
2021-09-10 12:24:44 +0300

    Littli code fix

---
+++ assets/js/theme/custom/customCheckout.js
@@ -0,0 +1,10 @@
+import PageManager from '../page-manager'
+
+export default class CustomDemo extends PageManager {
+    onReady () {
+
+        console.log("!@#!@#!@#!@#!@#!ASDASDASD");
+    }
+}
+console.log("!@#!@#!@#!@#!@#!ASDASDASD");
+alert('asdasdasd');
