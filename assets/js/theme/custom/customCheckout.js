cadd4fe538067fee7ffe018bee25c75f4106141b
yarovojigor1988 <yarovojigo1988@gmail.com>
2021-09-10 12:27:19 +0300

    one more fix

--- assets/js/theme/custom/customCheckout.js
+++ assets/js/theme/custom/customCheckout.js
@@ -3,8 +3,5 @@ import PageManager from '../page-manager'
 export default class CustomDemo extends PageManager {
     onReady () {
 
-        console.log("!@#!@#!@#!@#!@#!ASDASDASD");
     }
 }
-console.log("!@#!@#!@#!@#!@#!ASDASDASD");
-alert('asdasdasd');
